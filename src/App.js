f0a7252570a9a3a69b737dd79ef862908a1477c4
Diego Fontalvo <d.andres2002@hotmail.com>
2022-10-10 20:08:47 -0500

    Añadidos servicios de PokeAPI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import PokemonCard from './components/Card/PokemonCard';
+import { getPokemonData, getPokemonList } from './services/Pokemon.service';
+
 
 function App() {
+
+  const [paginatorURLs, setPaginatorURLs] = useState({
+    current: "https://pokeapi.co/api/v2/pokemon/",
+    next: null,
+    previous: null,
+  })
+  const [pokemonList, setPokemonList] = useState([])
+
+  useEffect(() => {  
+    loadList()
+  }, [])
+  
+
+  const loadList = async () => {
+    const list = await getPokemonList(paginatorURLs.current)
+    console.log(list);
+    const pokemonsResponses = await Promise.allSettled(list.results.map(pokemon => getPokemonData(pokemon.name)))
+    setPokemonList(pokemonsResponses.map(res => res.value))
+    setPaginatorURLs({
+      current: paginatorURLs.current,
+      next: paginatorURLs.next,
+      previous: paginatorURLs.previous
+    })
+  }
+
   return (
     <div>
+      {pokemonList.map(pokemon => <PokemonCard pokemon={pokemon}/>)}
     </div>
   );
 }
